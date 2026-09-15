a9924cb7253131b7da039ecfba1ce2d908fd5022
shahd faisal <66003305+shahd-v@users.noreply.github.com>
2026-09-15 16:31:06 +0300

    Add annotated string conversion

---
+++ python_variable_annotations/3-to_str.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""String conversion with annotations."""
+
+
+def to_str(n: float) -> str:
+    """Return the string representation of a floating-point value."""
+    return str(n)
