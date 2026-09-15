13ce32158a5465a9d58a9e6036290775627ae633
shahd faisal <66003305+shahd-v@users.noreply.github.com>
2026-09-15 16:31:02 +0300

    Add annotated string concatenation

---
+++ python_variable_annotations/1-concat.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""String concatenation with annotations."""
+
+
+def concat(str1: str, str2: str) -> str:
+    """Return two strings joined together."""
+    return str1 + str2
