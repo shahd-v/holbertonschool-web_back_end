81111649fd4d30d310768132130d39a04e11db20
shahd faisal <66003305+shahd-v@users.noreply.github.com>
2026-09-15 16:31:04 +0300

    Add annotated floor function

---
+++ python_variable_annotations/2-floor.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""Flooring annotated floating-point values."""
+
+import math
+
+
+def floor(n: float) -> int:
+    """Return the integer floor of a floating-point value."""
+    return math.floor(n)
