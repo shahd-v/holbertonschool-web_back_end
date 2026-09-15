f3455c2528e028ae5d3206b53c1336a4008a1806
shahd faisal <66003305+shahd-v@users.noreply.github.com>
2026-09-15 16:31:09 +0300

    Add typed float list sum

---
+++ python_variable_annotations/5-sum_list.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""Summing a typed list of numbers."""
+
+from typing import List
+
+
+def sum_list(input_list: List[float]) -> float:
+    """Return the sum of all values in a list of floats."""
+    return sum(input_list)
